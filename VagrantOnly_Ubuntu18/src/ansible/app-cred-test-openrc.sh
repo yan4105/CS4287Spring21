f84350c8c1968a10edcd1ba2ffd4495ab7890342
yan4105 <71930350+yan4105@users.noreply.github.com>
2021-04-03 04:44:11 -0500

    Delete VagrantOnly_Ubuntu18 directory

--- VagrantOnly_Ubuntu18/src/ansible/app-cred-test-openrc.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-export OS_AUTH_TYPE=v3applicationcredential
-export OS_AUTH_URL=https://kvm.tacc.chameleoncloud.org:5000/v3
-export OS_IDENTITY_API_VERSION=3
-export OS_REGION_NAME="KVM@TACC"
-export OS_INTERFACE=public
-export OS_APPLICATION_CREDENTIAL_ID=de7f4bd3235e48d29dcea00ad4aa05c9
-export OS_APPLICATION_CREDENTIAL_SECRET=3eI0egPDjS5VidLBZqJ7-gIhytlvCNW-I0V6eyfW3ZdgaSsjy3KIwBmieMpcS-h_p_0hnDwPPrmuqfktRMhWKA
